4b7b749c7deb4b07721589d20e46e24cf7b66d88
Mouad Anani <manani@bookenda.ca>
2018-03-13 21:44:39 -0400

    wesh

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/', usersController);
 app.use('/', imagesController);
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
