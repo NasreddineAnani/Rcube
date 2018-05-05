e30b41b610544e7807a767ffd38de969503beb09
abdouoi <abdou_oi@yahoo.fr>
2018-05-05 16:24:20 -0400

    implement centersController routes

--- backend/controller/centersController.js
+++ backend/controller/centersController.js
@@ -1,24 +1,39 @@
 const express = require('express');
+const request = require('request');
+
 const firebase = require('firebase');
 const admin = require('firebase-admin');
 
 const router = express.Router();
 const db = admin.firestore();
 
-router.get('/centers/:id', function (req, res) {
-  let id = req.params.id;
-  // appeler bd postgre
+const host = 'http://10.212.32.104:5000';
+
+router.get('/nearestCenter', function (req, res) {
+  // get nearest center with address
+  console.log(req.query.address);
+  let address = req.query.address;
+  let url = host + "/nearestCenter/" + address;
+  console.log(url);
+  request(url, (error, result, body) => {
+    console.log(body);
+    res.status(200).send({"nearestCenter": body});
+
+  });
 });
 
-router.get('/centers', function (req, res) {
-  // get nearest center wit adress
-});
-
-// get when thash coming by address
+// get when trash coming by address
 router.get('/trashDay', function (req, res) {
-  // get nearest center wit adress
-});
+  console.log(req.query.address);
+  let address = req.query.address;
+  let url = host + "/trashDay/" + address;
+  console.log(url);
+  request(url, (error, result, body) => {
+    console.log(body);
+    res.status(200).send({"trashDay": body});
 
+  });
+});
 
 
 module.exports = router;
